c07466bd6c736ea2f54eabd84c05c258b0b8b63c
amazimgboycot <amazimgboycot@github.com>
2018-04-21 21:29:10 +0800

    处理index页面业务逻辑

---
+++ src/components/about.vue
@@ -0,0 +1,60 @@
+<template>
+ <div class="root">
+  <ul v-for="(item,index) in listData" :key = "index">
+   <li>
+    <span>{{item.name}}</span>
+    <div @click = "getItem(index, i)" :class = "el.isSelect?'active':'' " v-for = "(el,i) in item.itemData" :key = "i">
+     {{el.name}}
+    </div>
+   </li>
+  </ul>
+ </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      listData: [
+        {name: '1', itemData: [{name: '11', isSelect: false}, {name: '12', isSelect: false}, {name: '13', isSelect: false}]},
+        {name: '2', itemData: [{name: '21', isSelect: false}, {name: '22', isSelect: false}, {name: '23', isSelect: false}]},
+        {name: '3', itemData: [{name: '31', isSelect: false}, {name: '32', isSelect: false}, {name: '33', isSelect: false}]}
+      ]
+    }
+  },
+  methods: {
+    getItem (index, i) {
+    this.listData.forEach((el,idx)=>{
+        if(index === idx){
+          el.itemData.forEach((v,id)=>{
+           if(id === i){
+             v.isSelect = true;
+           } else {
+               v.isSelect = false;
+            } 
+          })
+        }
+      })
+    //   console.log(index, i)
+    }
+  }
+}
+</script>
+<style scoped>
+    li{
+        display:inline-block;
+    }
+    li>span{
+        display:block;
+        background:red;
+    }
+    li>div{
+        background:green;
+        margin:10px;
+        padding:30px 50px;
+        display:inline-block;
+    }
+    li>div.active{
+        background:orange;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/home'
 import Edit from '@/components/home/edit'
+import About from '@/components/about'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path: '/edit',
       name: 'edit',
       component: Edit
+    }, {
+      path: '/about',
+      name: 'about',
+      component: About
     }
   ]
 })
